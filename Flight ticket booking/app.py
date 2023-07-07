4c60917bd47e0e6360c94c44c0ff72c5d7925286
Lekha L <101722556+lekhacse@users.noreply.github.com>
2023-07-07 07:54:28 +0530

    Update app.py

--- Flight ticket booking/app.py
+++ Flight ticket booking/app.py
@@ -2,55 +2,54 @@
 
 from flask import Flask, render_template, request, session, redirect
 from werkzeug.security import generate_password_hash, check_password_hash
-import sqlite3
+from models import User, Flight, Booking
+from helpers import get_user_by_username, get_flight_by_id, get_bookings_by_user
+import config
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # Set a secret key for session management
-DATABASE = 'flight_booking.db'
+app.secret_key = config.SECRET_KEY
 
 
-# User registration
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+
         # Perform data validations
 
         # Generate password hash
         password_hash = generate_password_hash(password)
 
-        # Store user in the database
+        # Create new user
+        user = User(username=username, password=password_hash)
+        user.save()
 
         return redirect('/login')
     return render_template('signup.html')
 
 
-# User login
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
 
-        # Retrieve user from the database based on username
-
-        if user and check_password_hash(user['password'], password):
-            session['username'] = user['username']
+        user = get_user_by_username(username)
+        if user and check_password_hash(user.password, password):
+            session['username'] = user.username
             return redirect('/dashboard')
 
         return render_template('login.html', error='Invalid username or password')
     return render_template('login.html')
 
 
-# User logout
 @app.route('/logout')
 def logout():
     session.pop('username', None)
     return redirect('/login')
 
 
-# Add flight
 @app.route('/add_flight', methods=['GET', 'POST'])
 def add_flight():
     if 'username' not in session:
@@ -62,65 +61,72 @@ def add_flight():
         arrival_time = request.form['arrival_time']
         seat_count = request.form['seat_count']
 
-        # Store flight in the database
+        flight = Flight(flight_number=flight_number, departure_time=departure_time, arrival_time=arrival_time,
+                        seat_count=seat_count)
+        flight.save()
 
         return redirect('/dashboard')
     return render_template('add_flight.html')
 
 
-# Remove flight
 @app.route('/remove_flight/<flight_id>')
 def remove_flight(flight_id):
     if 'username' not in session:
         return redirect('/login')
 
-    # Remove flight from the database based on flight_id
+    flight = get_flight_by_id(flight_id)
+    if flight:
+        flight.delete()
 
     return redirect('/dashboard')
 
 
-# Flight search
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     if request.method == 'POST':
         date = request.form['date']
         time = request.form['time']
 
-        # Fetch flights from the database based on date and time
+        flights = Flight.query.filter(Flight.departure_time >= date + ' ' + time).all()
 
         return render_template('search_results.html', flights=flights)
     return render_template('search.html')
 
 
-# Flight details and booking
 @app.route('/flight/<flight_id>', methods=['GET', 'POST'])
 def flight_details(flight_id):
     if 'username' not in session:
         return redirect('/login')
 
+    flight = get_flight_by_id(flight_id)
+    if not flight:
+        return redirect('/search')
+
     if request.method == 'POST':
         # Perform seat availability check
-
-        if seats_available:
+        if flight.seat_count > 0:
             # Book ticket
-            # Update seat count in the database
+            booking = Booking(user=session['username'], flight_number=flight.flight_number,
+                              departure_time=flight.departure_time, arrival_time=flight.arrival_time)
+            booking.save()
+
+            # Update seat count
+            flight.seat_count -= 1
+            flight.save()
 
             return redirect('/my_bookings')
         else:
             return render_template('booking_error.html')
 
-    # Retrieve flight details from the database based on flight_id
-
     return render_template('flight_details.html', flight=flight)
 
 
-# User bookings
 @app.route('/my_bookings')
 def my_bookings():
     if 'username' not in session:
         return redirect('/login')
 
-    # Retrieve user's bookings from the database
+    bookings = get_bookings_by_user(session['username'])
 
     return render_template('my_bookings.html', bookings=bookings)
 
